dc2ca7b908d16f35c1174cc2879b428e2a7ce6aa
Onumagabriel <onuma-gabriel.onyemaechi@Binghamuni.edu.ng>
2024-05-22 00:12:58 +0100

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,8 @@
 package User;
 
 public class Student extends User {
-    String matric_no = "bhu/22/04/09/0016";
-    String admission_no = "302283749741";
+    String matric_no = "bhu/22/04/05/0102";
+    String admission_no = "302245652237";
     String registerCourse() {
         if (matric_no.equals("") || admission_no.equals("")) {
             return "You cannot register at this moment";
@@ -14,13 +14,13 @@ public class Student extends User {
         if (matric_no.equals("") || admission_no.equals("")) {
             return "Cannot check your result";
         } else {
-            return "You got 6As and 1C";
+            return "You got 6As and 1B";
         }
     }
     public static void main(String[] args) {
         Student student = new Student();
-        student.name = "Josiah Shehu";
-        student.password = "Josycode@123";
+        student.name = "Onuma Gabriel";
+        student.password = "gabriel2004";
         System.out.println(student.register());
         System.out.println(student.registerCourse());
         System.out.println(student.checkResults());
